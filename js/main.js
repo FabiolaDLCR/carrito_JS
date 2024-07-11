90b40ee33916bd224a7b4e7c4057651482e1c3f5
Fabiola de la Cruz <itzeel.1504@gmail.com>
2024-07-10 23:36:48 -0600

    estructura lista

--- js/main.js
+++ js/main.js
@@ -8,14 +8,14 @@ const productos = [
     },
     {
         id: 2,
-        nombre: "popote de acero inoxidable",
+        nombre: "Popote de acero inoxidable",
         precio: 35,
         cantidad:0,
         img: "./images/straw.jpg"
     },
     {
         id: 3,
-        nombre: "infusor de acero inoxidable",
+        nombre: "Infusor de acero inoxidable",
         precio: 45,
         cantidad:0,
         img: "./images/infusor.jpg"
@@ -42,79 +42,13 @@ let productsContainer = document.getElementById("products-container")
 function renderProductos (productsArray) {
     productsArray.forEach(producto => {
         const card = document.createElement("div")
-        card.innerHTML = `<img src="${producto.img}" alt="">
+        card.innerHTML = `<img class= "product-img" src="${producto.img}" alt="">
                           <h3>${producto.nombre}</h3>
                           <p>$${producto.precio}.00</p>
-                          <div class="boton-agregar">
-                          <button class="productoAgregar" id="${producto.id}">Agregar </button>
-                          </div>`
+                          <button class="add-btn" id="${producto.id}"> Agregar </button>
+                        `
         productsContainer.appendChild(card)
     })
     addToCartButton()
 }
 renderProductos(productos)
-
-let botonMenos = document.querySelector('.boton-menos');
-let botonMas = document.querySelector('.boton-mas');
-let userInput = document.querySelector('.cantidad');
-
-let userProducts = 0;
- 
-botonMas.addEventListener('click', (e)=>{
-      userProducts++;
-      userInput.value = userProducts;
-});
-
-botonMenos.addEventListener('click', (e)=>{
-  userProducts--;
-    if(userProducts <= 0){
-    userProducts = 0;
-    }
-    userInput.value = userProducts
-});
-
-function addToCartButton () {
-    addButton = document.querySelectorAll(".productoAgregar")
-    addButton.forEach(button => {
-        button.onclick = (e) => {
-            const productId = e.currentTarget.id
-            const selectedProduct = productos.find(producto => producto.id == productId)
-  
-            cartProducts.push(selectedProduct)
-            console.log(cartProducts)
-
-            localStorage.setItem("cartProducts", JSON.stringify(cartProducts))
-        }
-    })
-}
-addToCartButton()
-
-
-// function restarSumar () {
-//    let userProducts=0;
-
-//    addButton = document.querySelector(".boton-menos")
-//    addButton.forEach(button =>{
-//     button.addEventListener('click', () => {
-//         userProducts--;
-//         if(userProducts <= 0){
-//          userProducts = 0;
-//         }
-//         userInput.value = userProducts;
-//      });
-//    })
-//    addButton = document.querySelector(".boton-mas")
-//    addButton.forEach(button =>{
-//     button.addEventListener('click', () => {
-//         userProducts++;
-//         userInput.value = userProducts;
-//      });
-//    })
-//    }
-// restarSumar()
-
-
-
-
-
-
