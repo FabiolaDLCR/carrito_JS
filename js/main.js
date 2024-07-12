9262c13d9bff6ff8e30586dd4cd142daec1ef48c
Fabiola de la Cruz <itzeel.1504@gmail.com>
2024-07-12 01:22:38 -0600

    botones listos

--- js/main.js
+++ js/main.js
@@ -36,34 +36,143 @@ const productos = [
     }
 ] 
 
-let cartProducts = []
-
+let cartBtn = document.querySelector('.cart');
+let cartModal = document.querySelector ('.cart-modal-container');
+let closeCartBtn = document.querySelector ('.close-btn');
 let productsContainer = document.getElementById("products-container")
+let listCartProducts = document.querySelector('.cart-list')
+let cartProducts = [];
+
+
+///////////////PRODUCT LIST//////////////////////////////
 function renderProductos (productsArray) {
     productsArray.forEach(producto => {
         const card = document.createElement("div")
+        card.dataset.id = producto.id;
         card.innerHTML = `<img class= "product-img" src="${producto.img}" alt="">
                           <h3>${producto.nombre}</h3>
                           <p>$${producto.precio}.00</p>
                           <button class="add-btn" id="${producto.id}"> Agregar </button>
                         `
         productsContainer.appendChild(card)
-    })   
+    })  
 }
+
 renderProductos(productos)
 
-//SHOW CART MODAL//
-// let iconCart=document.querySelector('.cart');
-// let body = document.querySelector('body');
 
-// iconCart.addEventListener('click',() => {
-// body.classList.toggle('.display-cart')
-// })
-//SHOW CART MODAL//
+////////HIDDE AND SHOW MODAL//////////////////////
 
-const cartBtn = document.querySelector('.cart');
-const cartModal = document.querySelector ('.cart-modal');
-cartBtn = addEventListener ('click', () => {
-    cartModal.computedStyleMap.display = 'block';
-    
+cartBtn.addEventListener ('click', () => {
+    cartModal.style.display = ('block');
 })
+closeCartBtn.addEventListener ('click', () => {
+    cartModal.style.display = ('none')
+})
+
+productsContainer.addEventListener ('click', (e) => {
+    let positionClick = e.target;
+    if(positionClick.classList.contains('add-btn')){
+        let productId = positionClick.parentElement.dataset.id;
+        addToCart (productId);
+    }
+} )
+
+////////ADD TO CART BUTTON//////////////////////
+let addToCart = (productId) => {
+    let positionInCart = cartProducts.findIndex((value) => value.productId == productId);
+    if(cartProducts.length <= 0){
+    cartProducts = [{
+        productId: productId,
+        quantity:1
+    }]
+}
+else if(positionInCart<0)
+    {
+cartProducts.push({
+    productId: productId,
+    quantity:1
+});}
+else{
+    cartProducts[positionInCart].quantity = cartProducts[positionInCart].quantity + 1;
+}
+cartHTML();
+////////CART STORAGE//////////////////////
+cartMemory();
+}
+let cartMemory = () => {
+    localStorage.setItem('cartProduct', JSON.stringify(cartProducts));
+
+}
+
+
+
+////////CART PRODUCTS//////////////////////
+let cartHTML = () => {
+    listCartProducts.innerHTML = ``;
+    if(cartProducts.length > 0){
+        cartProducts.forEach(cartProduct => {
+            let newCart = document.createElement('div');
+            newCart.classList.add('item');
+            newCart.dataset.id = cartProduct.productId;
+            let positionProduct= productos.findIndex((value) => value.id == cartProduct.productId);
+            let info = productos[positionProduct];
+            newCart.innerHTML = `
+             <div class="image">
+             <img src="${info.img}" alt="cartThumbnail">
+             </div>
+             <div class="product-name">
+              ${info.nombre}
+             </div>
+             <div class="Price">
+              $${info.precio * cartProduct.quantity}
+             </div>
+             <div class="quantity">
+              <span class="minus">-</span>
+             <span>${cartProduct.quantity}</span>
+              <span class="plus">+</span>
+             </div>
+      `
+         listCartProducts.appendChild(newCart);
+        })
+    }
+}
+
+listCartProducts.addEventListener ('click', (e) => {
+    let positionClick = e.target;
+    if(positionClick.classList.contains('minus') || positionClick.classList.contains('plus')){
+        let productId = positionClick.parentElement.parentElement.dataset.id;
+        let type = 'minus';
+        if(positionClick.classList.contains('plus')){
+            type = 'plus';   
+        }
+        changeQuantity(productId, type);
+    }
+})
+////////////////MINUS AND ADD ITEMS BUTTONS///////////////////////////
+let changeQuantity = (productId,type) => {
+    let positionItemInCart= cartProducts.findIndex((value) => value.productId == productId);
+    if (positionItemInCart >= 0){
+        switch(type){
+            case 'plus':
+            cartProducts[positionItemInCart].quantity = cartProducts[positionItemInCart].quantity =1;
+            break;
+            default:
+                let valueChange=cartProducts[positionItemInCart].quantity -1;
+                if(valueChange > 0){
+                    cartProducts[positionItemInCart].quantity = valueChange;
+                }
+                else{
+                    cartProducts.slice(positionItemInCart,1)
+                }
+                break;
+        }
+    }
+    cartMemory();
+    cartHTML();
+}
+
+if(localStorage.getItem('cartProduct')){
+    cartProducts = JSON.parse(localStorage.getItem('cartProduct'));
+    cartHTML();
+}
