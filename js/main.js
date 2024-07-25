c7ed4bfc977a84536cb09e4e10d0f8eeab5ddf43
Fabiola de la Cruz <itzeel.1504@gmail.com>
2024-07-25 14:10:46 -0600

    archivo JSON agregado

--- js/main.js
+++ js/main.js
@@ -1,64 +1,12 @@
-const productos = [
-    {
-        id: 1, 
-        nombre: "Frasco de vidrio hermético",
-        precio: 120, 
-        cantidad:0,
-        img: "./images/frasco-vidrio.jpg"
-    },
-    {
-        id: 2,
-        nombre: "Popote de acero inoxidable",
-        precio: 35,
-        cantidad:0,
-        img: "./images/straw.jpg"
-    },
-    {
-        id: 3,
-        nombre: "Infusor de acero inoxidable",
-        precio: 45,
-        cantidad:0,
-        img: "./images/infusor.jpg"
-    },
-    {
-        id: 4,
-        nombre: "Bolsa de tela",
-        precio: 345,
-        cantidad:0,
-        img: "./images/totebag.jpg"
-    },
-    {
-        id: 5,
-        nombre: "Termo 450ml",
-        precio: 360,
-        cantidad:0,
-        img: "./images/termo-inox.jpg"
-    }
-] 
 
 let cartBtn = document.querySelector('.cart');
 let cartModal = document.querySelector ('.cart-modal-container');
 let closeCartBtn = document.querySelector ('.close-btn');
 let productsContainer = document.getElementById("products-container")
-let listCartProducts = document.querySelector('.cart-list')
+let listCartProducts = document.querySelector('.cart-list');
 let cartProducts = [];
-
-
-///////////////PRODUCT LIST//////////////////////////////
-function renderProductos (productsArray) {
-    productsArray.forEach(producto => {
-        const card = document.createElement("div")
-        card.dataset.id = producto.id;
-        card.innerHTML = `<img class= "product-img" src="${producto.img}" alt="">
-                          <h3>${producto.nombre}</h3>
-                          <p>$${producto.precio}.00</p>
-                          <button class="add-btn" id="${producto.id}"> Agregar </button>
-                        `
-        productsContainer.appendChild(card)
-    })  
-}
-
-renderProductos(productos)
+let carts = [];
+let products = [];
 
 
 ////////HIDDE AND SHOW MODAL//////////////////////
@@ -70,15 +18,53 @@ closeCartBtn.addEventListener ('click', () => {
     cartModal.style.display = ('none')
 })
 
+//////////////////// ADD PRODUCTS LIST TO HTML/////////////////////
+let productsToHTML = () => {
+    if (products.length > 0){
+        products.forEach(product => {
+            let newProduct = document.createElement('div');
+                newProduct.dataset.id = product.id;
+                newProduct.classList.add('item');
+                newProduct.innerHTML = 
+                            `<img class= "product-img" src="${product.img}" alt="">
+                             <h3>${product.nombre}</h3>
+                             <p>$${product.precio}.00</p>
+                             <button class="add-btn" id="${product.id}"> Agregar </button>`;
+                         productsContainer.appendChild(newProduct);
+        });
+        
+    }
+}
+
+///////////////////JSON PRODUCTS//////////////////////// 
+
+let initJSON = () =>{
+    fetch("./db/products.JSON")
+    .then(response => response.json())
+    .then(data => {
+        products=data;
+        productsToHTML();
+     if(localStorage.getItem('cartProducts')){
+        cartProducts = JSON.parse(localStorage.getItem('cartProducts'));
+        cartHTML();
+        }
+    })
+    }
+initJSON();
+
+
+    
+
+////////ADD TO CART BUTTON//////////////////////
 productsContainer.addEventListener ('click', (e) => {
     let positionClick = e.target;
     if(positionClick.classList.contains('add-btn')){
         let productId = positionClick.parentElement.dataset.id;
         addToCart (productId);
+        
     }
 } )
 
-////////ADD TO CART BUTTON//////////////////////
 let addToCart = (productId) => {
     let positionInCart = cartProducts.findIndex((value) => value.productId == productId);
     if(cartProducts.length <= 0){
@@ -87,27 +73,27 @@ let addToCart = (productId) => {
         quantity:1
     }]
 }
-else if(positionInCart<0)
-    {
+else if(positionInCart < 0){
 cartProducts.push({
     productId: productId,
     quantity:1
-});}
+});
+}
 else{
     cartProducts[positionInCart].quantity = cartProducts[positionInCart].quantity + 1;
 }
+
+
 cartHTML();
 ////////CART STORAGE//////////////////////
 cartMemory();
 }
 let cartMemory = () => {
     localStorage.setItem('cartProduct', JSON.stringify(cartProducts));
-
 }
 
 
-
-////////CART PRODUCTS//////////////////////
+////////ADD PRODUCTS TO CART AND HTML//////////////////////
 let cartHTML = () => {
     listCartProducts.innerHTML = ``;
     if(cartProducts.length > 0){
@@ -115,8 +101,8 @@ let cartHTML = () => {
             let newCart = document.createElement('div');
             newCart.classList.add('item');
             newCart.dataset.id = cartProduct.productId;
-            let positionProduct= productos.findIndex((value) => value.id == cartProduct.productId);
-            let info = productos[positionProduct];
+            let positionProduct= products.findIndex((value) => value.id == cartProduct.productId);
+            let info = products[positionProduct];
             newCart.innerHTML = `
              <div class="image">
              <img src="${info.img}" alt="cartThumbnail">
@@ -135,7 +121,7 @@ let cartHTML = () => {
       `
          listCartProducts.appendChild(newCart);
         })
-    }
+    } 
 }
 
 listCartProducts.addEventListener ('click', (e) => {
@@ -149,6 +135,8 @@ listCartProducts.addEventListener ('click', (e) => {
         changeQuantity(productId, type);
     }
 })
+
+
 ////////////////MINUS AND ADD ITEMS BUTTONS///////////////////////////
 let changeQuantity = (productId, type) => {
     let positionItemInCart= cartProducts.findIndex((value) => value.productId == productId);
@@ -171,8 +159,19 @@ let changeQuantity = (productId, type) => {
     cartMemory();
     cartHTML();
 }
+/////////////////////LIBRERIAS////////////////////////////////
+            Toastify({
+                text: "Envio gratis en compras mayores a $499",
+                duration: 5500,
+                destination: "https://github.com/apvarun/toastify-js",
+                newWindow: true,
+                gravity: "top", // `top` or `bottom`
+                position: "center", // `left`, `center` or `right`
+                stopOnFocus: true, // Prevents dismissing of toast on hover
+                style: {
+                  background: "linear-gradient(to right, #00b09b, #96c93d)",
+                },
+                onClick: function(){} // Callback after click
+              }).showToast();
+    
 
-if(localStorage.getItem('cartProduct')){
-    cartProducts = JSON.parse(localStorage.getItem('cartProduct'));
-    cartHTML();
-}
